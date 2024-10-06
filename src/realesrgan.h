b360a6c34dde91f2562bc87d853e155dc86b258b
k4yt3x <i@k4yt3x.com>
2024-10-06 00:00:00 +0000

    feat: added export macros so Windows libraries will be built
    Signed-off-by: k4yt3x <i@k4yt3x.com>

--- src/realesrgan.h
+++ src/realesrgan.h
@@ -3,41 +3,50 @@
 #ifndef REALESRGAN_H
 #define REALESRGAN_H
 
+#ifdef WIN32
+#ifdef LIBREALSERGAN_EXPORTS
+#define LIBREALSERGAN_API __declspec(dllexport)
+#else
+#define LIBREALSERGAN_API __declspec(dllimport)
+#endif
+#else
+#define LIBREALSERGAN_API
+#endif
+
 #include <string>
 
 // ncnn
-#include "net.h"
 #include "gpu.h"
 #include "layer.h"
+#include "net.h"
 
-class RealESRGAN
-{
-public:
+class LIBREALSERGAN_API RealESRGAN {
+   public:
     RealESRGAN(int gpuid, bool tta_mode = false);
     ~RealESRGAN();
 
 #if _WIN32
-    int load(const std::wstring& parampath, const std::wstring& modelpath);
+    int load(const std::wstring &parampath, const std::wstring &modelpath);
 #else
-    int load(const std::string& parampath, const std::string& modelpath);
+    int load(const std::string &parampath, const std::string &modelpath);
 #endif
 
-    int process(const ncnn::Mat& inimage, ncnn::Mat& outimage) const;
+    int process(const ncnn::Mat &inimage, ncnn::Mat &outimage) const;
 
-public:
+   public:
     // realesrgan parameters
     int scale;
     int tilesize;
     int prepadding;
 
-private:
+   private:
     ncnn::Net net;
-    ncnn::Pipeline* realesrgan_preproc;
-    ncnn::Pipeline* realesrgan_postproc;
-    ncnn::Layer* bicubic_2x;
-    ncnn::Layer* bicubic_3x;
-    ncnn::Layer* bicubic_4x;
+    ncnn::Pipeline *realesrgan_preproc;
+    ncnn::Pipeline *realesrgan_postproc;
+    ncnn::Layer *bicubic_2x;
+    ncnn::Layer *bicubic_3x;
+    ncnn::Layer *bicubic_4x;
     bool tta_mode;
 };
 
-#endif // REALESRGAN_H
+#endif  // REALESRGAN_H
